00a7625708e490c3df7ed6a5cc6bb3a8f4f92bc2
joeybodoia <jbodoia21@cmc.edu>
2025-03-07 15:19:22 -0400

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        futura: ['Futura', 'Futura-Medium', 'Futura-Bold', 'Trebuchet MS', 'Arial', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
